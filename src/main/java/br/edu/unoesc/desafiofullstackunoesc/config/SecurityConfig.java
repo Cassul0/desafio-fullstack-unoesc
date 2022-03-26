e263535ce0287a0cf3c6d1e63360cec59eafaa85
Cassul0 <gabriel.c@unoesc.edu.br>
2022-03-26 12:50:05 -0300

    Primeiro Commit
    Inclusão de dependências (spring security, thymeleaf, webjars, bootstrap)
    Criação do controller de login
    Inclusão do CSS
    Criação da página de login
    Tratamento de erro ao inserir credenciais inválidas

---
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.edu.unoesc.desafiofullstackunoesc.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			// acessos públicos liberados
+			.antMatchers("/webjars/**", "/css/**", "/js/**").permitAll()//libera acesso aos recursos
+			.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated()
+			.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/", true)
+				.failureUrl("/login-error")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/");
+	}
+}
---
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/web/controller/LoginController.java
@@ -0,0 +1,25 @@
+package br.edu.unoesc.desafiofullstackunoesc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	// abrir pagina login
+	@GetMapping({"/","/login"})
+	public String login() {
+		
+		return "login";
+	}	
+	
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Crendenciais inválidas!");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+		return "login";
+	}	
+
+}
